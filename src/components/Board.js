70d1f9b0fc400c8d63e4427552146ed9c2c61bee
Stefan Avlijas <52627970+avlijastefan@users.noreply.github.com>
2020-06-12 20:40:51 +0200

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Square from './Square';
+
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '400px',
+    height: '400px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplateRows: '1fr 1fr 1fr',
+    gridTemplateColumns: '1fr 1fr 1fr'
+ 
+};
+
+const Board = ({ squares, onClick }) => (
+<div style={style}>
+    {squares.map((square, i) => (
+    <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
+</div>
+);
+
+export default Board;
